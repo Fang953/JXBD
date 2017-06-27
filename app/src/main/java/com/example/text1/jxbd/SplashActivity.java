4301b6e53ebe656b32ac383e1934818434a5e240
Fang953 <381636062@qq.com>
2017-06-27 22:59:50 +0800

    add res

--- app/src/main/java/com/example/text1/jxbd/SplashActivity.java
+++ app/src/main/java/com/example/text1/jxbd/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.example.text1.jxbd;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 
 /**
  * Created by Administrator on 2017/6/27.
@@ -12,6 +14,19 @@ public class SplashActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //闪屏的activity
         setContentView(R.layout .activity_splash);
+        mCountDownTimer.start();
     }
+    CountDownTimer mCountDownTimer = new CountDownTimer(3000,1000){
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+    };
 }
