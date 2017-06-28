bd0e8e4eaa4da192da118068b3f12a7c52cd3914
Fang953 <381636062@qq.com>
2017-06-28 16:22:33 +0800

    将activity放到对应的包里面

--- app/src/main/java/com/example/text1/jxbd/activity/MainActivity.java
+++ app/src/main/java/com/example/text1/jxbd/activity/MainActivity.java
@@ -1,13 +1,16 @@
-package com.example.text1.jxbd;
+package com.example.text1.jxbd.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.text1.jxbd.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/text1/jxbd/activity/SplashActivity.java
+++ app/src/main/java/com/example/text1/jxbd/activity/SplashActivity.java
@@ -1,10 +1,12 @@
-package com.example.text1.jxbd;
+package com.example.text1.jxbd.activity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+
+import com.example.text1.jxbd.R;
 
 /**
  * Created by Administrator on 2017/6/27.
