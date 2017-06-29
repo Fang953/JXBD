8c4c9fee9513990e726a9e8d263a628a34b601a0
Fang953 <381636062@qq.com>
2017-06-29 23:48:10 +0800

    通过okhttp下载考卷数据

--- app/src/main/java/com/example/text1/jxbd/activity/MainActivity.java
+++ app/src/main/java/com/example/text1/jxbd/activity/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.text1.jxbd.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.text1.jxbd.R;
+import com.example.text1.jxbd.bean.SubjectTitle;
+import com.example.text1.jxbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void test(View view) {
+        OkHttpUtils<SubjectTitle> utils =new OkHttpUtils<>(getApplicationContext());
+        String url="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils .url(url)
+                .targetClass(SubjectTitle.class)
+                .execute(new OkHttpUtils.OnCompleteListener<SubjectTitle>() {
+                    @Override
+                    public void onSuccess(SubjectTitle result) {
+                        Log.e("main","result="+result );
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
         startActivity(new Intent(MainActivity.this ,ExamActivity .class ));
+
     }
 
     public void exit(View view) {
