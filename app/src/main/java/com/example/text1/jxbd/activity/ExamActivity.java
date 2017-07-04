33ac9d568337feda55707733757b4cc18159e901
Fang953 <381636062@qq.com>
2017-07-04 10:48:28 +0800

    加载考试题，实现选项的初始化

--- app/src/main/java/com/example/text1/jxbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/text1/jxbd/activity/ExamActivity.java
@@ -10,6 +10,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -30,8 +32,10 @@ import java.util.List;
  */
 
 public class ExamActivity extends AppCompatActivity{
-    LinearLayout layoutLoading;
-    TextView tvSubjectTitle,tvQuestionTitle,tvOption1,tvOption2,tvOption3,tvOption4,tvLoad;
+    TextView tvSubjectTitle,tvQuestionTitle,tvOption1,tvOption2,tvOption3,tvOption4,tvLoad,tvNo;
+    CheckBox cb01,cb02,cb03,cb04;
+    CheckBox[] cbArray=new CheckBox[4];
+    LinearLayout layoutLoading,layout03,layout04;
     ImageView jkImageView;
     ProgressBar dialog;
     IExamBiz biz;
@@ -51,8 +55,8 @@ public class ExamActivity extends AppCompatActivity{
         mLoadExamBroadcast = new LoadExamBroadcast();
         mLoadQuestionBroadcast =new LoadQuestionBroadcast();
         setListener();
-        biz=new QuestionBiz();
         initView();
+        biz=new QuestionBiz();
         loadData();
     }
 
@@ -75,13 +79,24 @@ public class ExamActivity extends AppCompatActivity{
 
     private void initView() {
         layoutLoading =(LinearLayout) findViewById(R.id.layout_loading);
+        layout03 =(LinearLayout) findViewById(R.id.layout_03);
+        layout04 =(LinearLayout) findViewById(R.id.layout_04);
         dialog =(ProgressBar) findViewById(R.id.load_dialog);
         tvSubjectTitle= (TextView) findViewById(R.id.tv_subjecttitle);
         tvQuestionTitle= (TextView) findViewById(R.id.tv_question_title);
+        tvNo= (TextView) findViewById(R.id.tv_question_no);
         tvOption1= (TextView) findViewById(R.id.tv_option1);
         tvOption2= (TextView) findViewById(R.id.tv_option2);
         tvOption3= (TextView) findViewById(R.id.tv_option3);
         tvOption4= (TextView) findViewById(R.id.tv_option4);
+        cb01 = (CheckBox) findViewById(R.id.cb_01);
+        cb02 = (CheckBox) findViewById(R.id.cb_02);
+        cb03 = (CheckBox) findViewById(R.id.cb_03);
+        cb04 = (CheckBox) findViewById(R.id.cb_04);
+        cbArray[0] = cb01;
+        cbArray[1] = cb02;
+        cbArray[2] = cb03;
+        cbArray[3] = cb04;
         tvLoad = (TextView) findViewById(R.id.tv_load);
         jkImageView = (ImageView ) findViewById(R.id.im_exam_image) ;
         layoutLoading .setOnClickListener(new View.OnClickListener() {
@@ -90,7 +105,41 @@ public class ExamActivity extends AppCompatActivity{
                 loadData();
             }
         });
+        cb01.setOnCheckedChangeListener(listener);
+        cb02.setOnCheckedChangeListener(listener);
+        cb03.setOnCheckedChangeListener(listener);
+        cb04.setOnCheckedChangeListener(listener);
     }
+
+    CompoundButton.OnCheckedChangeListener listener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(isChecked) {
+                int userAnswer = 0;
+                switch (buttonView.getId()) {
+                    case R.id.cb_01:
+                        userAnswer = 1;
+                        break;
+                    case R.id.cb_02:
+                        userAnswer = 2;
+                        break;
+                    case R.id.cb_03:
+                        userAnswer = 3;
+                        break;
+                    case R.id.cb_04:
+                        userAnswer = 4;
+                        break;
+                }
+                Log.e("checkedChanged","usera=" + userAnswer + ",isChecked="+isChecked );
+                if (userAnswer > 0) {
+                    for (CheckBox cb : cbArray) {
+                        cb.setChecked(false);
+                    }
+                    cbArray[userAnswer - 1].setChecked(true);
+                }
+            }
+        }
+    };
 
     private void initData() {
         if(isLoadExamInfoReceiver && isLoadQuestionsReceiver){
@@ -100,28 +149,43 @@ public class ExamActivity extends AppCompatActivity{
                 if (subjectTitle != null) {
                     showData(subjectTitle);
                 }
-                List<Question> questionList = ExamApplication.getInstance().getQuestionList();
-                if (questionList != null) {
-                    showQuestion(questionList);
-                }
+                showQuestion(biz.getQuestion());
             }else{
-                dialog.setVisibility(View.GONE);
+                layoutLoading.setEnabled(true);
+                dialog.setVisibility(View.GONE);  //设置隐藏
                 tvLoad.setText("下载失败，点击重新下载");
             }
         }
     }
 
-    private void showQuestion(List<Question> questionList) {
-        Question question = questionList.get(0);
+    private void showQuestion(Question question){
+        Log.e("showQuestion","showQuestion,question="+question);
         if(question != null){
+            tvNo .setText(biz.getQuestionIndex());
             tvQuestionTitle .setText(question .getQuestion());
             tvOption1 .setText(question .getItem1());
             tvOption2 .setText(question .getItem2());
             tvOption3 .setText(question .getItem3());
             tvOption4 .setText(question .getItem4());
-            Picasso .with(ExamActivity.this)
-                    .load(question .getUrl())
-                    .into(jkImageView);
+            layout03.setVisibility(question.getItem3().equals("")? View.GONE:View.VISIBLE);
+            cb03.setVisibility(question.getItem3().equals("")?View .GONE :View .VISIBLE );
+            layout04.setVisibility(question.getItem4().equals("")? View.GONE:View.VISIBLE);
+            cb04.setVisibility(question.getItem4().equals("")?View .GONE :View .VISIBLE );
+            if(question.getUrl()!=null && !question.getUrl().equals("")) {
+                jkImageView .setVisibility(View.VISIBLE);
+                Picasso.with(ExamActivity.this)
+                        .load(question.getUrl())
+                        .into(jkImageView);
+            }else{
+                jkImageView .setVisibility(View.GONE);
+            }
+            resetOptions();
+        }
+    }
+
+    private void resetOptions() {
+        for(CheckBox cb : cbArray){
+            cb.setChecked(false);
         }
     }
 
@@ -138,6 +202,18 @@ public class ExamActivity extends AppCompatActivity{
         if (mLoadQuestionBroadcast!=null){
             unregisterReceiver(mLoadQuestionBroadcast);
         }
+    }
+
+    public void preQuestion(View view) {
+        showQuestion(biz.preQuestion());
+    }
+
+    public void nextQuestion(View view) {
+        showQuestion(biz.nextQuestion());
+    }
+
+    public void commit(View view){
+
     }
 
     //广播一
